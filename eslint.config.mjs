ce5dc2fbcfc9adef0c7ed976eb7bd127c3f4a360
SumanMishra2004 <suman132j@gmail.com>
2025-05-22 11:31:17 +0530

    error fixed try

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,9 @@ const eslintConfig = [
   {
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
+    "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-       "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
       "@next/next/no-img-element": "off",
     },
   },
